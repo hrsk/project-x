7c52cd15b989f2f6abcf095d3fdc4090c80b3717
hrsk <hrsk.yegor@gmail.com>
2025-08-12 11:18:24 +0300

    feat: использование react хуков createContext/useContext, самописный хук useTheme для изменения темы, css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import './index.scss'
+import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
 import {Suspense} from "react";
 import {Loading} from "./components/Loading";
 import {MainLazy} from "./components/pages/main/Main.lazy";
 import {AboutLazy} from "./components/pages/about/About.lazy";
+import {useTheme} from "./styles/theme/useTheme";
 
 export const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className={"app"}>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>toggleTheme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
-            <Suspense fallback={<Loading />}>
+            <Suspense fallback={<Loading/>}>
                 <Routes>
                     <Route path={'/'} element={<MainLazy/>}/>
                     <Route path={'/about'} element={<AboutLazy/>}/>
